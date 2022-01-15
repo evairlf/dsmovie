dee327597157e8d2a60fc76cfd550dd6b67e22d4
evairlf <evairleitefeldmann@gmail.com>
2022-01-15 15:19:25 -0300

    Props

--- backend/src/main/java/com/feldmann/dsmovie/repositories/UserRepository.java
+++ backend/src/main/java/com/feldmann/dsmovie/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.feldmann.dsmovie.repositories;
 
-import com.feldmann.dsmovie.entities.Movie;
 import com.feldmann.dsmovie.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
